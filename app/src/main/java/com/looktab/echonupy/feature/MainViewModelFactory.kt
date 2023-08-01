dd9321f1604774233d0574db6d540a1e4dbb78c4
yuchocopie <pointu341@gmail.com>
2023-08-02 05:25:39 +0900

    MainViewModelFactory setting

--- app/src/main/java/com/looktab/echonupy/feature/MainViewModelFactory.kt
+++ app/src/main/java/com/looktab/echonupy/feature/MainViewModelFactory.kt
@@ -3,9 +3,10 @@ package com.looktab.echonupy.feature
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import com.looktab.echonupy.data.source.nft.NftDataSource
 
-class MainViewModelFactory : ViewModelProvider.Factory {
+class MainViewModelFactory(val nftService: NftDataSource) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
-        return MainViewModel() as T
+        return MainViewModel(nftService) as T
     }
 }
